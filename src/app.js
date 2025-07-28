fa95a118faa2d384f572dbac76afc05982b52417
Abhishek Agrawal <86093849+Abhisek730@users.noreply.github.com>
2025-07-28 13:48:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,34 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
+// CORS setup
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5500", "https://admin.aapbihar.org"];
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
+app.options("*", cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
+
+// Middleware
 app.use(express.json());
-app.use(
-    cors({
-        origin: ["http://localhost:5173", "http://localhost:5500","https://admin.aapbihar.org"], // TODO: change this
-        credentials: true,
-    })
-);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
 import wingRoutes from "./routes/wing.route.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import volunteerRouter from "./routes/volunteer.routes.js";
 import memberRouter from "./routes/member.routes.js";
 import userRouter from "./routes/user.routes.js";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 import dashboardRouter from "./routes/dashboard.routes.js";
 
 app.get("/", (req, res) => {
