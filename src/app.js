83de735c23598cd483ab782957a7b9d8ac57c4a2
premprakashgupta <prem.com0011@gmail.com>
2025-08-25 18:12:28 +0530

    adding live url in cors

--- src/app.js
+++ src/app.js
@@ -47,6 +47,8 @@ app.use(
             "http://localhost:5500",
             "http://127.0.0.1:5501",
             "http://127.0.0.1:5500",
+            "https://aapbihar.org",
+            "https://admin.aapbihar.org",
         ], // TODO: change this
         credentials: true,
     })
