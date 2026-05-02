81817954a707e7c7eaa035ac2db83a98edf7eed3
premprakashgupta <prem.com0011@gmail.com>
2026-05-02 12:36:57 +0530

    test: allow all origins to bypass code-level CORS issues

--- src/app.js
+++ src/app.js
@@ -31,18 +31,7 @@ console.log(allowedOrigins)
 
 app.use(
     cors({
-        origin: (origin, callback) => {
-            // Allow requests with no origin (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                console.log("❌ CORS Blocked Origin:", origin);
-                console.log("✅ Allowed Origins:", allowedOrigins);
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: (origin, callback) => callback(null, true),
         credentials: true,
     })
 );
